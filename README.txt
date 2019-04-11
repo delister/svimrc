67425425e253975e08b29e26bca6a4ecc0682aa3
Максим Скорых <st.delister@gmail.com>
2019-04-11 08:38:35 +0300

    Readme

---
+++ README.txt
@@ -0,0 +1,6 @@
+Install:
+`curl -s -L https://clck.ru/FZFT3 | bash`
+or
+`curl -s -L  https://raw.githubusercontent.com/delister/svimrc/master/install.sh | bash`
+
+
--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/delister/svimrc.git ./.vim
 
 echo "" >> .gitignore
-echo "#Vim settings exclude" >> .gitignore
+echo "# Vim settings exclude" >> .gitignore
 echo ".vim" >> .gitignore
